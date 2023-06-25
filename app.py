0272b718a7c3ab36b9d68170bf850ee53710a373
Stephen <stephen.kohlmann@gmail.com>
2023-06-25 21:58:26 +0100

    Updatin Delete

--- app.py
+++ app.py
@@ -24,10 +24,15 @@ def add_task():
         return redirect(url_for("task_list"))
     return render_template("add.html")
 
-@app.route("/delete/<int:task_id>")
+@app.route("/delete/<int:task_id>", methods=["POST"])
 def delete_task(task_id):
-    if 0 <= task_id < len(tasks):
-        tasks.pop(task_id)
+    try:
+        if 0 <= task_id < len(tasks):
+            tasks.pop(task_id)
+        else:
+            raise IndexError("Invalid task_id")
+    except Exception as e:
+        print("An error occurred:", e)
     return redirect(url_for("task_list"))
 
 if __name__ == "__main__":
